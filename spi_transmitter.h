586f670f17d714bee778a0b291c0e4a99bc18b04
Tom Malone <tmalone+git@gmail.com>
2014-08-08 19:59:55 -0400

    Added SPI module pin definitions.

--- spi_transmitter.h
+++ spi_transmitter.h
@@ -5,6 +5,13 @@
  * Simple SPI usage between two Atmel ATmega328 microcontrollers.
  */
 
-
+/* Register/Port/Pin Definitions */
+#define SPI_DDR					DDRB
+#define SPI_PORT				PORTB
+#define SPI_PIN					PINB
+#define SPI_SS					PB2			// Slave Select
+#define SPI_MOSI				PB3			// Master Out/Slave In
+#define SPI_MISO				PB4			// Master In/Slave Out
+#define SPI_SCK					PB5			// Shift Clock
 
 void init_SPI(void);
