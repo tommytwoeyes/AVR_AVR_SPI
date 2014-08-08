905dd20968c25a14e2d1dd71596937bd5e0267ad
Tom Malone <tmalone+git@gmail.com>
2014-08-08 19:25:20 -0400

    init_SPI(): enabled SPI module by turning off SPI power reduction mode.

--- spi_transmitter.c
+++ spi_transmitter.c
@@ -4,7 +4,16 @@
  * 
  * Simple SPI usage between two Atmel ATmega328 microcontrollers.
  */
+ 
+#ifndef _AVR_SPI_XMIT_H
+#define _AVR_SPI_XMIT_H 1
+
+#include <avr/io.h>
 
 
+void init_SPI(void) {
+	// Enable SPI by turning off PRSPI bit in Power Reduction Register
+	PRR &= ~(1 << PRSPI);
+}
 
-void init_SPI(void);
+#endif /* _AVR_SPI_XMIT_H */
