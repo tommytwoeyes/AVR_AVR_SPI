11472eafcc659df7644aeae972cce2184b2f1f8b
Tom Malone <tmalone+git@gmail.com>
2014-08-08 20:01:08 -0400

    Configured SPI module pins SCK, MOSI and SS as output pins.

--- spi_transmitter.c
+++ spi_transmitter.c
@@ -9,11 +9,20 @@
 #define _AVR_SPI_XMIT_H 1
 
 #include <avr/io.h>
-
+#include "spi_transmitter.h"
 
 void init_SPI(void) {
 	// Enable SPI by turning off PRSPI bit in Power Reduction Register
 	PRR &= ~(1 << PRSPI);
+	
+	// Config SCK and MOSI for output
+	SPI_DDR |= (1 << SPI_SCK) | (1 << SPI_MOSI);
+	
+	// Config SS as output 
+	// (see datasheet and App Note AVR151 for setting SS as input in 
+	// SPI multi-master mode. Things get more complicated for SS in 
+	// that mode).
+	SPI_DDR |= (1 << SPI_SS);
 }
 
 #endif /* _AVR_SPI_XMIT_H */
