9312d7fffb44ddca65d0e446b50ad50295ab9788
Tom Malone <tmalone+git@gmail.com>
2014-08-09 19:26:54 -0400

    Defined macros for configuring AVR in MASTER and SLAVE mode.

--- spi_master.h
+++ spi_master.h
@@ -7,10 +7,13 @@
 
 #include "spi_mnemonics.h"
 
-/* SLAVE SELECT/DESELECT Macros */
+/* Macros */
+#define MASTER_MODE()		(SPCR |= (1 << MSTR);
+#define SLAVE_MODE()		(SPCR &= ~(1 << MSTR);
+
 // Signal initiation of communication cycle (Slave Select pin low)
-#define SLAVE_SELECT	SPI_PORT &= ~(1 << SPI_SS); 
+#define SLAVE_SELECT()		SPI_PORT &= ~(1 << SPI_SS); 
 // Signal end of communication cycle (Slave Select pin high)
-#define SLAVE_DESELECT	SPI_PORT |= (1 << SPI_SS);
+#define SLAVE_DESELECT()	SPI_PORT |= (1 << SPI_SS);
 
 void init_SPI(void);
